c1c4cc35582d8b9bd50de21a436652d749c21513
Sarvin Shrivastava <sarvin5124@gmail.com>
2024-06-10 16:16:30 +0530

    completed the laptop web view
    completed the css code for web view on a laptop screen

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,11 +1,21 @@
-document.getElementById("copy-button").addEventListener("click", function() {
-      const textToCopy = "sarvin5124@gmail.com";
-      const textArea = document.createElement("textarea");
-      textArea.value = textToCopy;
-      document.body.appendChild(textArea);
-      textArea.select();
-      document.execCommand("copy");
-      document.body.removeChild(textArea);
-      alert("Email has been copied!");
-  });
-  
+document.getElementById("email-copy-button").addEventListener("click", function () {
+    const textToCopy = "sarvin5124@gmail.com";
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Email has been copied!");
+});
+
+document.getElementById("contact-copy-button").addEventListener("click", function () {
+    const textToCopy = "+91 9310524050";
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Contact has been copied!");
+});
