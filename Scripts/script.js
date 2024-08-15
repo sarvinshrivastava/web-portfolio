c4c720e02e5a9af3f3dc5bc784c66a58988bd0e7
Sarvin Shrivastava <sarvin5124@gmail.com>
2024-08-15 16:54:46 +0530

    Blogs & Contact
    Added a contact me page and modified the blogs page to the ui. Now stuck in a basic issue that the 'My Projects' head is not allining center to the screen

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,31 +1,46 @@
-document.getElementById("email-copy-button").addEventListener("click", function () {
-    const textToCopy = "sarvin5124@gmail.com";
-    const textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-    alert("Email has been copied!");
-});
+// document.getElementById("email-copy-button").addEventListener("click", function () {
+//     const textToCopy = "sarvin5124@gmail.com";
+//     const textArea = document.createElement("textarea");
+//     textArea.value = textToCopy;
+//     document.body.appendChild(textArea);
+//     textArea.select();
+//     document.execCommand("copy");
+//     document.body.removeChild(textArea);
+//     alert("Email has been copied!");
+// });
 
-document.getElementById("contact-copy-button").addEventListener("click", function () {
-    const textToCopy = "+91 9310524050";
-    const textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-    alert("Contact has been copied!");
-});
+// document.getElementById("contact-copy-button").addEventListener("click", function () {
+//     const textToCopy = "+91 9310524050";
+//     const textArea = document.createElement("textarea");
+//     textArea.value = textToCopy;
+//     document.body.appendChild(textArea);
+//     textArea.select();
+//     document.execCommand("copy");
+//     document.body.removeChild(textArea);
+//     alert("Contact has been copied!");
+// });
 
+// function myFunction() {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+// }
 
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+  // Clear the input field when focused
+  function clearField(field) {
+    if (field.defaultValue == field.value) {
+        field.value = '';
+        field.style.color = '#000';
+    }
+  }
+
+  // Restore the input field when blured
+  function restoreField(field) {
+    if (field.value.trim() === '') {
+      field.value = field.defaultValue;
+      field.style.color = '#6b7280';
     }
   }
